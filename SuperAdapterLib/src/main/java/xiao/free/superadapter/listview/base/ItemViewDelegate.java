0daaba549a7cc7c61b72907a236f62d5c26fd2b6
CRGT_xiaoguochang <516282426@qq.com>
2018-10-07 19:54:28 +0800

    初始化工程

---
+++ SuperAdapterLib/src/main/java/xiao/free/superadapter/listview/base/ItemViewDelegate.java
@@ -0,0 +1,20 @@
+package xiao.free.superadapter.listview.base;
+
+
+import xiao.free.superadapter.listview.ViewHolder;
+
+/**
+ * Created by zhy on 16/6/22.
+ */
+public interface ItemViewDelegate<T>
+{
+
+    public abstract int getItemViewLayoutId();
+
+    public abstract boolean isForViewType(T item, int position);
+
+    public abstract void convert(ViewHolder holder, T t, int position);
+
+
+
+}
